7072c49c45189092699d9ce2d92d0dd23483a860
Motiejus Jakštys <motiejus@uber.com>
2018-01-26 13:27:17 +0200

    Put receiver into sender
    Terminal 1:
    
      $ go run ./sender_tcp.go --bind
    
    Terminal 2:
      $ nc localhost 1234
    
    Terminal 3:
        $ go run ./sender_tcp.go --takeover
    
    Terminal 4:
        $ nc localhost 1234
    
    Terminal 1:
        $ go run ./sender_tcp.go --takeover
    
    Terminal 5:
        $ nc localhost 1234

--- example/sender_tcp/sender_tcp.go
+++ example/sender_tcp/sender_tcp.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"flag"
-	"io"
+	"fmt"
 	"log"
 	"net"
 	"os"
@@ -13,72 +13,111 @@ import (
 	"github.com/ftrvxmtrx/fd"
 )
 
+const (
+	port   = 1234
+	socket = "/tmp/sendfd.sock"
+)
+
 var (
-	port   int
-	socket string
+	bind     bool
+	takeover bool
 )
 
 func init() {
-	flag.IntVar(&port, "p", 1234, "listen port")
-	flag.StringVar(&socket, "s", "/tmp/sendfd.sock", "socket")
+	flag.BoolVar(&bind, "bind", false, "bind on :1234")
+	flag.BoolVar(&takeover, "takeover", false, "take fd from "+socket)
 }
 
 func main() {
-	flag.Parse()
-
-	if !flag.Parsed() || socket == "" {
+	if flag.Parse(); !flag.Parsed() {
 		flag.Usage()
 		os.Exit(1)
 	}
 
-	tcpl, err := net.Listen("tcp", ":"+strconv.Itoa(port))
-	if err != nil {
-		log.Fatalf("tcp listen failed: %s", err)
+	if bind == takeover {
+		flag.Usage()
+		fmt.Fprintln(os.Stderr, "either bind or takeover is required and are mutually exclusive")
+		os.Exit(1)
 	}
-	defer tcpl.Close()
-	var wg sync.WaitGroup
-	open := true
-	go accepter(&wg, &open, tcpl)
 
-	var l net.Listener
-	l, err = net.Listen("unix", socket)
-	if err != nil {
-		log.Fatal("unix listen failed: %s\n", err)
+	var (
+		sockL  net.Listener
+		tcpL   net.Listener
+		wg     sync.WaitGroup
+		closed bool
+		err    error
+	)
+
+	if bind {
+		sockL, err = net.Listen("unix", socket)
+		if err != nil {
+			log.Fatalf("unix listen failed: %s\n", err)
+		}
+		if tcpL, err = net.Listen("tcp", ":"+strconv.Itoa(port)); err != nil {
+			log.Fatalf("tcp listen failed: %s\n", err)
+		}
+		defer tcpL.Close()
+	} else {
+		c, err := net.Dial("unix", socket)
+		if err != nil {
+			log.Fatal("net.Dial failed: %s\n", err)
+		}
+		defer c.Close()
+
+		var fs []*os.File
+		fs, err = fd.Get(c.(*net.UnixConn), 2, []string{"f1", "f2"})
+		if err != nil {
+			log.Fatalf("GET failed: %s\n", err)
+		}
+		log.Println("fds received")
+		defer fs[0].Close()
+		defer fs[1].Close()
+
+		if tcpL, err = net.FileListener(fs[0]); err != nil {
+			log.Fatalf("error converting tcpL: %s\n", err)
+		}
+
+		if sockL, err = net.FileListener(fs[1]); err != nil {
+			log.Fatalf("error converting sockL: %s\n", err)
+		}
 	}
-	defer l.Close()
+	go accepter(&wg, &closed, tcpL)
 
-	var a net.Conn
-	a, err = l.Accept()
+	var sockA net.Conn
+	sockA, err = sockL.Accept()
 	if err != nil {
 		log.Fatalf("unix accept failed: %s\n", err)
 	}
-	defer a.Close()
+	defer sockA.Close()
 
-	listenConn := a.(*net.UnixConn)
+	var tcpF *os.File // will be closed immediately after Put
+	if tcpF, err = tcpL.(*net.TCPListener).File(); err != nil {
+		log.Fatalf("tcp File() failed: %s\n", err)
+	}
 
-	var f *os.File
-	f, err = tcpl.(*net.TCPListener).File()
-	if err != nil {
-		log.Fatal("tcp File() failed: %s\n", err)
+	var sockLF *os.File // will be closed immediately after Put
+	if sockLF, err = sockL.(*net.UnixListener).File(); err != nil {
+		log.Fatalf("unix socket File() failed: %s\n", err)
 	}
-	if err = fd.Put(listenConn, f); err != nil {
-		log.Fatal("Put failed: %s\n", err)
+	if err = fd.Put(sockA.(*net.UnixConn), tcpF, sockLF); err != nil {
+		log.Fatalf("Put failed: %s\n", err)
 	}
-	log.Println("socket transfered, closing tcpl and waiting for termination")
-	open = false
-	tcpl.Close()
-	f.Close()
+	sockLF.Close()
+	tcpF.Close()
+
+	log.Println("sockets transfered, draining")
+	closed = true
 	wg.Wait()
-	log.Println("drain complete, closing")
+	log.Println("drain complete, quitting")
 }
 
-func accepter(wg *sync.WaitGroup, open *bool, tcpl net.Listener) {
-	for *open {
+func accepter(wg *sync.WaitGroup, closed *bool, tcpL net.Listener) {
+	for !*closed {
 		var c net.Conn
 		var err error
 		log.Println("accepting...")
-		c, err = tcpl.Accept()
-		if !*open {
+		c, err = tcpL.Accept()
+		if *closed {
 			log.Println("stopping accept loop")
 			return
 		}
@@ -93,7 +132,7 @@ func accepter(wg *sync.WaitGroup, open *bool, tcpl net.Listener) {
 
 func echo(wg *sync.WaitGroup, c net.Conn) {
 	for {
-		if _, err := io.WriteString(c, "hello from sender\n"); err != nil {
+		if _, err := fmt.Fprintf(c, "hello from %d\n", os.Getpid()); err != nil {
 			log.Printf("write failed: %s\n", err.Error())
 			c.Close()
 			wg.Done()
